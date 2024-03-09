9fce99c4b712ba74884b024e240d27d16c484365
iSammy <ayenisamuel155@gmail.com>
2024-03-09 20:43:08 +0000

    landing page done

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { TopnavComponent } from '../topnav/topnav.component';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [SidebarComponent, TopnavComponent],
+  imports: [SidebarComponent, TopnavComponent, HomeComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
